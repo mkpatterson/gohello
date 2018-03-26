3543e5fb47c30751dbf301020f58aff547ee39a7
mkpatterson <mkp@dataspoke.tech>
2018-03-26 11:13:02 -0400

    added funcreturnloop

---
+++ funcreturnloop/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func wrapper() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
+//func main() {
+//	inc := wrapper()
+//	fmt.Println(inc())
+//	fmt.Println(inc())
+//}
+
+func main() {
+	inc := wrapper()
+	for i := 0; i < 5; i++ {
+		fmt.Println(inc())
+	}
+}
