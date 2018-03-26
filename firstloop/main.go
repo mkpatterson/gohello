53931d92c0553475a9a71598a60c7d46c4497af9
mkpatterson <mkp@dataspoke.tech>
2018-03-25 21:41:41 -0400

    Added UTF-8 to loop

--- firstloop/main.go
+++ firstloop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 2000; i++ {
-		fmt.Printf("%d \t %b \t %#x \n", i, i, i)
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %#x \t %q \n", i, i, i, i)
 	}
 }
