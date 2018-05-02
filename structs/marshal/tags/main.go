42d8d8bf09be168f120d8ae9d0b1dcabb23051ee
mkpatterson <mkp@dataspoke.tech>
2018-05-02 11:30:44 -0400

    marshalling and encoding

--- structs/marshal/tags/main.go
+++ structs/marshal/tags/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 )
 
 type Person struct {
 	First string
-	Last string `json:"-"`
-	Age int `json:"Wisdom Score"`
+	Last  string `json:"-"`
+	Age   int    `json:"Wisdom Score"`
 }
 
 func main() {
 	p1 := Person{"James", "Bond", 20}
 	bs, _ := json.Marshal(p1)
 	fmt.Println(string(bs))
-
 }
